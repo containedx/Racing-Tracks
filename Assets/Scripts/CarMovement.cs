88d00243aceebb94120b5a243f396508a5520af4
Kinga <niu.ula.a@poczta.fm>
2021-02-09 00:06:10 +0100

    Change Movement, Added Kind Of Rotation

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
-    public float speed = 1.0f;
+    public float speed = 5.0f;
+    public float torque = -2.0f;
 
     public Rigidbody2D rigidBody;
-
-    Vector2 moveVector;
-
     
     void Update()
     {
-        moveVector.x = Input.GetAxisRaw("Horizontal");
-        moveVector.y = Input.GetAxisRaw("Vertical");
+        
     }
 
     void FixedUpdate()
     {
-        rigidBody.MovePosition(rigidBody.position + moveVector * speed * Time.deltaTime);
+        //going forward movement
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up");
+            rigidBody.AddForce(transform.right * speed);
+        }
+
+        //rotation movement
+        rigidBody.AddTorque( Input.GetAxis("Horizontal") * torque);
     }
 }
